ae1ea9e7882bf602ab47cb1e6a77d7cd268fa97b
Ravi Ranjan <rav594.r@gmail.com>
2023-11-16 18:58:11 +0530

    intermediate react

--- steps/src/App-v1.js
+++ steps/src/App-v1.js
@@ -10,8 +10,7 @@ export default function App() {
   return (
     <div>
       <Steps />
-      <StepMessage step={1}>Pass in content</StepMessage>
-      <StepMessage step={1}>Read children prop</StepMessage>
+      <Steps />
     </div>
   );
 }
@@ -43,37 +42,25 @@ function Steps() {
             <div className={step >= 2 ? "active" : ""}>2</div>
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
-
-          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
+          <p className="message">
+            Step {step}: {messages[step - 1]}
+          </p>
           <div className="buttons">
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
-              <span>👈🏿 Previous</span>
-            </Button>
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
-              Next 👉
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </div>
-  );
-}
-
-function StepMessage({ step, children }) {
-  return (
-    <p className="message">
-      Step {step}: {children}
-    </p>
-  );
-}
-
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
