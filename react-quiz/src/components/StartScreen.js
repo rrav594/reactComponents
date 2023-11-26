176fa18e26742198327212a2424e99875302f3a5
Ravi Ranjan <rav594.r@gmail.com>
2023-11-26 15:26:00 +0530

    React Quiz

---
+++ react-quiz/src/components/StartScreen.js
@@ -0,0 +1,18 @@
+function StartScreen({ numQuestions, dispatch }) {
+  return (
+    <div className="start">
+      <h2>Welcome to react quiz.</h2>
+      <h3>{numQuestions} questions to test your React mastery</h3>
+      <button
+        className="btn btn-ui"
+        onClick={() => {
+          dispatch({ type: "start" });
+        }}
+      >
+        Lets Start
+      </button>
+    </div>
+  );
+}
+
+export default StartScreen;
